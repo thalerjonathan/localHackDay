c0b60e04fce1fcfb330648448ffb2bf7738f5d61
Jonathan <thaler.jonathan@gmail.com>
2018-12-01 14:30:15 +0000

    websockets working

--- server/app.py
+++ server/app.py
@@ -1,16 +1,26 @@
 from flask import Flask
 from flask import Flask, flash, redirect, render_template, request, session, abort
+from flask_socketio import SocketIO
 import os
  
 app = Flask(__name__)
-app.secret_key = "super secret key" #necessary to prevent runtime error when handling sessions
+
+# disable caching while in development mode
+app.config["CACHE_TYPE"] = "null"
+
+# necessary to prevent runtime error when handling sessions
+app.secret_key = "super secret key"
+
+# required for websockets
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
 @app.route('/')
 def home():
     if not session.get('logged_in'):
         return render_template('auth/login.html')
     else:
-        return render_template('hub/hub.html')
+        return render_template('auth/socket.html')
  
 @app.route('/login', methods=['POST'])
 def do_login():
@@ -19,12 +29,9 @@ def do_login():
             pw = request.form['password']
             user = request.form['username']
 
-            print (user)
-            print (pw)
-
             if pw == 'password' and user == 'admin':
                 session['logged_in'] = True
-                return render_template('hub/hub.html')
+                return render_template('auth/socket.html')
             else:
                 print ("wrong password!")
         else:
@@ -37,4 +44,16 @@ def hub():
     if not session.get('logged_in'):
         return render_template('auth/login.html')
     else:
-        return render_template('hub/hub.html')
+        return render_template('auth/socket.html')
+
+@socketio.on('json')
+def handle_json(json):
+    print('received json on websocket: ' + str(json))
+
+@socketio.on('message')
+def handle_message(message):
+    print('received message on websocket: ' + message)
+
+@socketio.on('my event')
+def handle_my_custom_event(json):
+    print('received json: ' + str(json))
