2bc7453bbae0141d955e5305404b2e342a3e913c
Jonathan <thaler.jonathan@gmail.com>
2018-12-01 12:09:55 +0000

    moved all to server

--- server/app.py
+++ server/app.py
@@ -1,7 +1,21 @@
 from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, abort
+import os
+ 
 app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return "Hello World!"
-
+ 
+@app.route('/')
+def home():
+    if not session.get('logged_in'):
+        return render_template('auth/login.html')
+    else:
+        return "Hello Boss!"
+ 
+@app.route('/login', methods=['POST'])
+def do_login():
+    if request.form['password'] == 'password' and request.form['username'] == 'admin':
+        session['logged_in'] = True
+        return "Hello Boss!"
+    else:
+        flash('wrong password!')
+    return home()
