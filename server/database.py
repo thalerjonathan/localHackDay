a504ccaf8e5e0a664ab8577a3f790d258f07d9c3
Kwabena Amponsah <kwabenantim@yahoo.com>
2018-12-01 13:48:55 +0000

    Initial DB

---
+++ server/database.py
@@ -0,0 +1,68 @@
+import sqlite3
+import hashlib
+
+# TODO LIST:
+# - Handle SQL Injection
+
+
+def db_connect(dbname='../database/database.db'):
+    return sqlite3.connect(dbname)
+
+
+def db_add_user(email, pass_plain, display_name):
+    pass_hash = db_hash_password(pass_plain)
+
+    conn = db_connect()
+
+    with conn:
+        cur = conn.cursor()
+        cur.execute('INSERT INTO users(email, password_hash, display_name)'
+                    'VALUES("{0}","{1}","{2}");'.format(email, pass_hash, display_name))
+
+
+def db_get_user(email):
+    conn = db_connect()
+
+    with conn:
+        cur = conn.cursor()
+        cur.execute('SELECT email, password_hash, display_name '
+                    'FROM users WHERE email = "{0}"'.format(email))
+        return cur.fetchone()
+
+
+def db_validate_user(email, pass_plain):
+    row = db_get_user(email)
+
+    if row:
+        if row[1] == db_hash_password(pass_plain):
+            return True
+
+    return False
+
+
+def db_user_exists(email):
+    row = db_get_user(email)
+
+    if row:
+        return True
+
+    return False
+
+
+def db_hash_password(pass_plain):
+    return hashlib.sha512(pass_plain).hexdigest()
+
+
+def main():
+    email = "james@hey.com"
+    password = "password"
+    display_name = "JJ"
+
+    print("User = {0} Exists? {1}".format(email, db_user_exists(email)))
+    print("Adding User {0}".format(email))
+    db_add_user(email, password, display_name)
+    print("User = {0} Exists? {1}".format(email, db_user_exists(email)))
+
+    wrong_password = "thispasswordiswrong"
+    print("User = {0}, Password = {1} Validates? ".format(email, db_validate_user(email, wrong_password), wrong_password))
+    print("User = {0}, Password = {1} Validates? ".format(email, db_validate_user(email, wrong_password), password))
