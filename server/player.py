1bc24ea19460512dc434824fc81fafaf8db64f76
James Hey <james@Jamess-MacBook-Pro.local>
2018-12-01 19:09:29 +0000

    player collision added

--- server/player.py
+++ server/player.py
@@ -1,6 +1,17 @@
 class Player:
-  def __init__(self, pid, name, x, y):
-    self.pid = pid
-    self.name = name
-    self.x = x
-    self.y = y
+    def __init__(self, pid, name, x, y):
+      self.pid = pid
+      self.name = name
+      self.x = x
+      self.y = y
+    
+    def check_collision(self,player):
+        if self.x == player.x :
+            if self.y == player.y:
+                return True
+        return False
+
+
+
+ 
+    
